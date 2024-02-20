53473fe22e669d529d2ff274f71332999e6c2691
Maju Facirolli <majufacirolli2004@hotmail.com>
2024-02-20 10:53:44 -0300

    class Cat

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       Cat kaiser = new Cat("Kaiser", 1, "black");
+       Cat panquequinhaDeBanana = new Cat("Panquequinha de Banana", 2, "orange");
+
+       kaiser.sleep();
+       panquequinhaDeBanana.play();
+    }
+}
